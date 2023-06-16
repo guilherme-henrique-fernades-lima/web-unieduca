6fa483cd6b39664211aa941c374e8f58a1f87c39
Davi Henrique <revondigitalbr@gmail.com>
2023-06-16 01:12:07 +0000

    DH - Vinculo na rota API

--- controller/api/apiController.js
+++ controller/api/apiController.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const usuariosController = require('./admin/usuariosController')
+const cmsController = require('./admin/cmsController')
 
 router.use('/usuarios',usuariosController)
+router.use('/cms',cmsController)
 
 module.exports = router
