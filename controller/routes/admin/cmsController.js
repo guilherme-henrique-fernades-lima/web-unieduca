163a34fe5f6587bb0d4fa3ba93c82f20ba93691e
Kaio Marques <kaiomarques2015xperia@gmail.com>
2023-06-20 22:15:02 +0000

    KM view serviços, videos e ajustes de rotas

--- controller/routes/admin/cmsController.js
+++ controller/routes/admin/cmsController.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Sobre = require('../../../Database/cms/Sobre')
 const Parceiro = require('../../../Database/cms/Parceiro')
+const Video = require('../../../Database/cms/Video')
+const Servico = require('../../../Database/cms/Servico')
 
 // /admin/cms/sobre
 router.get('/sobre',async(req,res)=>{
@@ -17,7 +19,7 @@ router.get('/sobre',async(req,res)=>{
 
 router.get('/parceiros',async(req,res)=>{
     try {
-        const parceiros = await Parceiro.findOne()
+        const parceiros = await Parceiro.findAll()
         res.render('admin/cms/parceiros',{parceiros:parceiros})
     } catch (error) {
         console.log(error)
@@ -25,5 +27,28 @@ router.get('/parceiros',async(req,res)=>{
         res.redirect('/admin')
     }
 })
+
+router.get('/video',async(req,res)=>{
+    try {
+        const videos = await Video.findAll()
+        res.render('admin/cms/videos',{videos:videos})
+    } catch (error) {
+        console.log(error)
+        req.flash('erro','Ocorreu um erro ao acessar, gentileza tente novamente! Caso o erro persista entre em contato com o suporte.')
+        res.redirect('/admin')
+    }
+})
+
+router.get('/servicos',async(req,res)=>{
+    try {
+        const servicos = await Servico.findAll()
+        res.render('admin/cms/servicos',{servicos:servicos})
+    } catch (error) {
+        console.log(error)
+        req.flash('erro','Ocorreu um erro ao acessar, gentileza tente novamente! Caso o erro persista entre em contato com o suporte.')
+        res.redirect('/admin')
+    }
+})
+
 
 module.exports = router
