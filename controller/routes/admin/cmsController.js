3c6edff13e7eef6f37c96bd2cae684a9d2e46245
Kaio Marques <kaiomarques2015xperia@gmail.com>
2023-06-20 18:12:17 +0000

    KM view sobre, parceiros e crião de rota parceiros

--- controller/routes/admin/cmsController.js
+++ controller/routes/admin/cmsController.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const Sobre = require('../../../Database/cms/Sobre')
+const Parceiro = require('../../../Database/cms/Parceiro')
 
 // /admin/cms/sobre
 router.get('/sobre',async(req,res)=>{
     try {
         const sobre = await Sobre.findOne()
         res.render('admin/cms/sobre',{sobre:sobre})
+    } catch (error) {
+        console.log(error)
+        req.flash('erro','Ocorreu um erro ao acessar, gentileza tente novamente! Caso o erro persista entre em contato com o suporte.')
+        res.redirect('/admin')
+    }
+})
+
+router.get('/parceiros',async(req,res)=>{
+    try {
+        const parceiros = await Parceiro.findOne()
+        res.render('admin/cms/parceiros',{parceiros:parceiros})
     } catch (error) {
         console.log(error)
         req.flash('erro','Ocorreu um erro ao acessar, gentileza tente novamente! Caso o erro persista entre em contato com o suporte.')
